a5983bcc2ad206fd5189d5f04a85b763d7826152
vwanyungu <vwanyungu254@gmail.com>
2026-07-27 21:09:42 +0300

    feat: authorization middleware

--- backend/v1/index.ts
+++ backend/v1/index.ts
@@ -3,6 +3,7 @@ import loginRouter from "./routers/loginRouter.js";
 import logoutRouter from "./routers/logoutRouter.js";
 import refreshTokenRouter from "./routers/refreshTokenRouter.js";
 import authenticateToken from "./middlewares/token.js";
+import authorize from "./middlewares/authorize.js";
 import forgotPasswordRouter from "./routers/forgotPasswordRouter.js";
 import resetUserPasswordRouter from "./routers/resetUserPasswordRouter.js";
 import usersRouter from "./routers/usersRouter.js";
@@ -12,6 +13,7 @@ import { authLimiter } from "../middlewares/rateLimit.js";
 const router = express.Router();
 
 router.use(authenticateToken());
+router.use(authorize());
 
 router.use("/v1/users", usersRouter);
 router.use("/v1/signup", authLimiter, usersRouter);
