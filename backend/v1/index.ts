486e27d5ff904b0a56b0dc58a3dae3b5131c3a7d
vwanyungu <vwanyungu254@gmail.com>
2026-07-27 23:52:51 +0300

    feat: employee CRUD
    fetch employees with filters, create/update/deactivate employee

--- backend/v1/index.ts
+++ backend/v1/index.ts
@@ -8,6 +8,7 @@ import forgotPasswordRouter from "./routers/forgotPasswordRouter.js";
 import resetUserPasswordRouter from "./routers/resetUserPasswordRouter.js";
 import usersRouter from "./routers/usersRouter.js";
 import teamsRouter from "./routers/teamsRouter.js";
+import employeesRouter from "./routers/employeesRouter.js";
 
 import { authLimiter } from "../middlewares/rateLimit.js";
 
@@ -18,6 +19,7 @@ router.use(authorize());
 
 router.use("/v1/users", usersRouter);
 router.use("/v1/teams", teamsRouter);
+router.use("/v1/employees", employeesRouter);
 router.use("/v1/signup", authLimiter, usersRouter);
 router.use("/v1/login", authLimiter, loginRouter);
 router.use("/v1/reset-password", authLimiter, resetUserPasswordRouter);
