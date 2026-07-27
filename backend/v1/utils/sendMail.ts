c1bb6a173b9cbc52f06a4fc8fca85fcfb906fbf2
vwanyungu <vwanyungu254@gmail.com>
2026-07-27 17:32:21 +0300

    feat: auth workflow
    Super admin can login
    User can signup, login, refresh token, reset password and logout

--- backend/v1/utils/sendMail.ts
+++ backend/v1/utils/sendMail.ts
@@ -1,16 +1,17 @@
-import nodemailer from 'nodemailer';
-import 'dotenv/config';
-import type { PasswordResetMail } from '../types.js';
+import nodemailer from "nodemailer";
+import "dotenv/config";
+import type { PasswordResetMail } from "../types.js";
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.sendgrid.net',
-  port: 587,
-  secure: false,
+  // host: "smtp.sendgrid.net",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
-      user: process.env.SENDGRID_USER,
-      pass: process.env.SENDGRID_PASSWORD
-  }
-})
+    user: process.env.SENDGRID_USER,
+    pass: process.env.SENDGRID_PASSWORD,
+  },
+});
 
 export const sendPasswordResetMail = async (mail: PasswordResetMail) => {
   const info = await transporter.sendMail({
@@ -20,21 +21,20 @@ export const sendPasswordResetMail = async (mail: PasswordResetMail) => {
     html: `<h1>Vunoh HR and Payroll Password Reset</h1>
       <p>Click the link below to reset your password:</p>
       <a href="${process.env.BACKEND_URL}/reset-password/${mail.resetToken}">Reset Password</a>
-      `
-  })
+      `,
+  });
 
   if (info.accepted.length === 0) {
-     return {
-        status: "error",
-        data: null,
-        message: "Failed to send email"
-      }
+    return {
+      status: "error",
+      data: null,
+      message: "Failed to send email",
+    };
   }
-  
+
   return {
     status: "success",
     data: info,
-    message: "Email sent successfully"
-  }
-}
-
+    message: "Email sent successfully",
+  };
+};
