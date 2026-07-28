9d46ef544f01c706d8ee525f53bfe24da75debdc
vwanyungu <vwanyungu254@gmail.com>
2026-07-28 22:50:41 +0300

    feat: initiate run endpoint

--- backend/v1/index.ts
+++ backend/v1/index.ts
@@ -13,6 +13,7 @@ import leaveTypesRouter from "./routers/leaveTypesRouter.js";
 import leaveRequestsRouter from "./routers/leaveRequestsRouter.js";
 import leaveBalancesRouter from "./routers/leaveBalancesRouter.js";
 import payslipsRouter from "./routers/payslipsRouter.js";
+import payrollRunsRouter from "./routers/payrollRunsRouter.js";
 
 import { authLimiter } from "../middlewares/rateLimit.js";
 
@@ -28,6 +29,7 @@ router.use("/v1/leave-types", leaveTypesRouter);
 router.use("/v1/leave-requests", leaveRequestsRouter);
 router.use("/v1/leave-balances", leaveBalancesRouter);
 router.use("/v1/payslips", payslipsRouter);
+router.use("/v1/payroll-runs", payrollRunsRouter);
 router.use("/v1/signup", authLimiter, usersRouter);
 router.use("/v1/login", authLimiter, loginRouter);
 router.use("/v1/reset-password", authLimiter, resetUserPasswordRouter);
